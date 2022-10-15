44fd875521b177e9018bd96872002a2063e38345
Matt Swieboda <mswieboda@users.noreply.github.com>
2022-10-15 12:25:07 -0500

    prep for manual territory and army allocation

--- src/risk/manager.cr
+++ src/risk/manager.cr
@@ -2,16 +2,20 @@ module Risk
   class Manager
     getter players : Array(Player)
     getter map : Map
-    getter phase : Symbol
+    getter? auto_allocate_territories
+    getter? auto_allocate_armies
     getter phase_index : UInt8
+    getter phase : Symbol
     getter turn_index : UInt8
     getter player : Player
 
     Phases = [:order, :allocation, :play]
 
-    def initialize(players, map)
+    def initialize(players, map, auto_allocate_territories = true, auto_allocate_armies = true)
       @players = players
       @map = map
+      @auto_allocate_territories = auto_allocate_territories
+      @auto_allocate_armies = auto_allocate_armies
 
       @phase_index = 0_u8
       @phase = Phases[@phase_index]
@@ -54,22 +58,41 @@ module Risk
 
     def allocate_territories
       empty_territories = map.territories.select(&.empty?)
-      territory = empty_territories.sample if empty_territories.any?
 
-      unless territory
-        player_territories = map.territories.select(&.player?(player))
-        territory = player_territories.sample if player_territories.any?
+      if empty_territories.any?
+        if auto_allocate_territories?
+          if territory = empty_territories.sample
+            if player.units > 0
+              territory.player = player
+              player.units -= 1
+              territory.units += 1
+            elsif turn_index == players.size - 1
+              next_phase
+            end
+
+            next_turn
+          end
+        else
+          # wait for user interaction
+        end
+      else
+        if auto_allocate_armies?
+          player_territories = map.territories.select(&.player?(player))
+          territory = player_territories.sample if player_territories.any?
+
+          if territory && player.units > 0
+            territory.player = player
+            player.units -= 1
+            territory.units += 1
+          elsif turn_index == players.size - 1
+            next_phase
+          end
+
+          next_turn
+        else
+          # wait for user interaction
+        end
       end
-
-      if territory && player.units > 0
-        territory.player = player
-        player.units -= 1
-        territory.units += 1
-      elsif turn_index == players.size - 1
-        next_phase
-      end
-
-      next_turn
     end
   end
 end
